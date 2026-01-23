8e6dcd444ea89e1efc65b18431ef6451e2a1926e
jlvcpa <dpi.acct@gmail.com>
2026-01-23 21:51:28 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -332,13 +332,14 @@ function renderQuestionImporterPage() {
     content.appendChild(container);
 
     // Call imported function
-    // if(typeof renderQuestionImporter === 'function') {
-    //     renderQuestionImporter('importer-container');
-    // } else {
-    //     container.innerHTML = `<div class="p-8 text-center text-gray-500">Importer module not loaded. Uncomment import in app.js</div>`;
-    // }
-    
-    // Placeholder content until file is linked
+     if(typeof renderQuestionImporter === 'function') {
+         renderQuestionImporter('importer-container');
+     } else {
+         container.innerHTML = `<div class="p-8 text-center text-gray-500">Importer module not loaded. Uncomment import in app.js</div>`;
+     }
+
+    /*
+     Placeholder content until file is linked
     container.innerHTML = `
         <div class="w-full max-w-4xl mx-auto p-8">
             <div class="bg-white rounded-lg shadow-sm border border-gray-200 p-8 text-center">
@@ -351,6 +352,7 @@ function renderQuestionImporterPage() {
             </div>
         </div>
     `;
+    */
 }
 
 // --- QUIZZES & ACTIVITIES PAGE RENDERER ---
@@ -360,15 +362,15 @@ function renderQuizzesActivitiesPage() {
     content.innerHTML = '';
 
     // UNCOMMENT AND USE THIS BLOCK WHEN quizzesAndActivities.js IS READY
-    /*
+   
     if (typeof renderQuizzesAndActivities === 'function') {
         renderQuizzesAndActivities(content, currentUser);
     } else {
         content.innerHTML = `<div class="p-8 text-center text-gray-500">Module not loaded properly.</div>`;
     }
-    */
-
+    
     // Placeholder content until file is linked
+    /*
     content.innerHTML = `
         <div class="w-full max-w-4xl mx-auto p-8">
             <div class="bg-white rounded-lg shadow-sm border border-gray-200 p-8 text-center">
@@ -381,6 +383,7 @@ function renderQuizzesActivitiesPage() {
             </div>
         </div>
     `;
+    */
 }
 
 // --- CREATOR PAGE RENDERER ---
@@ -390,9 +393,10 @@ function renderCreatorPage() {
     content.innerHTML = '';
 
     // If you have imported renderQuizActivityCreator, call it here:
-    // renderQuizActivityCreator(content);
+    renderQuizActivityCreator(content);
     
     // Placeholder content until file is linked
+    /*
     content.innerHTML = `
         <div class="w-full max-w-4xl mx-auto p-8">
             <div class="bg-white rounded-lg shadow-sm border border-gray-200 p-8 text-center">
@@ -405,6 +409,7 @@ function renderCreatorPage() {
             </div>
         </div>
     `;
+    */
 }
 
 function renderLandingPage() {
