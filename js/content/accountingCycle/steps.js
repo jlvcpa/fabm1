bd87c684623452ac10eae24590db5d85b0b3a5a8
jlvcpa <dpi.acct@gmail.com>
2026-02-05 21:00:13 +0800

    Update steps.js

--- js/content/accountingCycle/steps.js
+++ js/content/accountingCycle/steps.js
@@ -91,7 +91,8 @@ export const TaskSection = ({ step, activityData, answers, stepStatus, updateAns
                 </div>
 
                 ${/* 3. WORKSPACE */''}
-                <div className="bg-white rounded shadow-sm border border-gray-200 relative">
+                ${/* ADDED: mb-12 to ensure bottom content isn't cut off when scrolling */''}
+                <div className="bg-white rounded shadow-sm border border-gray-200 relative mb-12">
                     ${showEarlyWarning && html`<div className="absolute inset-0 bg-gray-50 bg-opacity-30 z-10 pointer-events-none"></div>`}
 
                     <${StepComponent} 
