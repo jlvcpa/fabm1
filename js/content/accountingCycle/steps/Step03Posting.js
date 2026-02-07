9a90013068c0d529715a8e9597dbfb8792d9c93e
jlvcpa <dpi.acct@gmail.com>
2026-02-08 07:16:35 +0800

    Update Step03Posting.js

--- js/content/accountingCycle/steps/Step03Posting.js
+++ js/content/accountingCycle/steps/Step03Posting.js
@@ -647,7 +647,7 @@ export default function Step03Posting({ activityData, data, onChange, showFeedba
         onChange('journalPRs', newPRs);
     };
 
-    // REMOVE 'FALSE &&' IN  ${false && showFeedback && html` TO UNHIDE THE BANNER
+    // REMOVE 'false &&' IN  ${false && showFeedback && html` TO UNHIDE THE BANNER
     return html`
         <div className="flex flex-col gap-4 h-full">
             ${showFeedback && html`
